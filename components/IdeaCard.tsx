bb90d9e9ca535a7d7958830f0fd9ccd917b0c366
“malcolmmutare” <mutarem@gmail.com>
2024-02-05 15:24:22 +0200

    Persistent Storage and Sample Ideas Code Improvement 🛠️🔍

--- components/IdeaCard.tsx
+++ components/IdeaCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as z from "zod";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -14,9 +14,9 @@ const EditIdeaSchema = z.object({
   title: z.string().min(5, {
     message: "⚠ Title must be at least 5 characters.",
   })
-  .max(16, {
-    message: "⚠ Title must not be longer than 16 characters.",
-  }),
+    .max(16, {
+      message: "⚠ Title must not be longer than 16 characters.",
+    }),
   description: z.string(),   //update later .optional()
   content: z.string().max(140, "Must be less than"),  //update later .optional()
   createdTime: z.date(),
@@ -26,36 +26,42 @@ type EditIdeaSchemaType = z.infer<typeof EditIdeaSchema>;
 
 interface IdeaCardProps {
   idea: Idea;
-  onDelete: () => void; 
+  onDelete: () => void;
   onSave: (updatedIdea: Idea) => void;
 
 }
 
 const IdeaCard: React.FC<IdeaCardProps> = ({ idea, onDelete, onSave }) => {
   const [charCount, setCharCount] = useState(0);
-  
-  const { 
-    register, 
-    handleSubmit, 
-    // setValue, 
+  const [isSaved, setIsSaved] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    setValue, 
     formState: { errors }
-    } = useForm<EditIdeaSchemaType>({
-      resolver: zodResolver(EditIdeaSchema),
-      mode: "onChange",
-      reValidateMode: "onChange",
-      shouldFocusError: true,
-      defaultValues: {
-        title: idea.title || '',
-        description: idea.description || '',
-        content: idea.content || '',
-      },
+  } = useForm<EditIdeaSchemaType>({
+    resolver: zodResolver(EditIdeaSchema),
+    mode: "onChange",
+    reValidateMode: "onChange",
+    shouldFocusError: true,
+    defaultValues: {
+      title: idea.title || '',
+      description: idea.description || '',
+      content: idea.content || '',
+    },
   });
 
-const onSubmit: SubmitHandler<Idea> = async (data) => {
-  try {
-    const updatedIdea: Idea = EditIdeaSchema.parse({
-      ...idea,
-        uuid: uuidv4(),
+  useEffect(() => {
+    setValue('title', idea.title || '');
+    setValue('description', idea.description || '');
+    setValue('content', idea.content || '');
+  }, [idea, setValue]);
+
+  const onSubmit: SubmitHandler<Idea> = async (data) => {
+    try {
+      const updatedIdea: Idea = EditIdeaSchema.parse({
+        ...idea,
         title: data.title,
         description: data.description,
         content: data.content,
@@ -63,6 +69,8 @@ const onSubmit: SubmitHandler<Idea> = async (data) => {
       });
 
       onSave(updatedIdea);
+      setIsSaved(true);
+      setTimeout(() => setIsSaved(false), 3000);
       // toast notif
     } catch (error) {
       if (error instanceof z.ZodError) {
@@ -73,68 +81,65 @@ const onSubmit: SubmitHandler<Idea> = async (data) => {
         // toast notif
       }
     }
-};
+  };
 
 
 
   return (
-    <form  className="relative bg-white rounded-lg shadow-lg p-4 mb-4 lg:w-1/4">
-        
-        <button
-          onClick={onDelete}
-          className="absolute top-0 right-0 p-2 cursor-pointer text-red-500"
-        >
-          <MdDeleteForever className="text-red-500 text-4xl"/>
-        </button>
-        
-        <div>
-          <input
-            {...register('title')}
-            type="text"
-            className="font-bold text-lg mb-2 focus:outline-none focus:shadow-outline border-b-2 border-blue-500 w-full"
-            placeholder="Enter title..."
-            autoFocus
-          />
-          {errors.title && <span className="text-red-500">{errors.title.message}</span>}
+    <form className={`relative bg-white rounded-lg shadow-lg p-4 mb-4 lg:w-1/4 ${isSaved ? 'bg-green-100' : ''}`} >
+      <button
+        onClick={onDelete}
+        className="absolute top-0 right-0 p-2 cursor-pointer text-red-500"
+      >
+         <MdDeleteForever className="text-red-500 text-4xl" />
+      </button>
+      <div>
+        <input
+          {...register('title')}
+          type="text"
+          className="font-bold text-lg mb-2 focus:outline-none focus:shadow-outline border-b-2 border-blue-500 w-full"
+          placeholder="Enter title..."
+          autoFocus
+        />
+        {errors.title && <span className="text-red-500">{errors.title.message}</span>}
 
-          <textarea
-            {...register('description')}
-            className="resize-none focus:outline-none focus:shadow-outline w-full"
-            placeholder="Idea description..."
-            />
-          {errors.description && <span className="text-red-500">{errors.description.message}</span>}
+        <textarea
+          {...register('description')}
+          className="resize-none focus:outline-none focus:shadow-outline w-full"
+          placeholder="Idea description..."
+        />
+        {errors.description && <span className="text-red-500">{errors.description.message}</span>}
 
-          <textarea
-            {...register('content')}
-            className="resize-none focus:outline-none focus:shadow-outline w-full"
-            placeholder="Enter your ideas here..."
-            defaultValue={idea.content}
-            onChange={(e) => {
-              const newContent = e.target.value;
-              setCharCount(newContent.length);
-            }}
-          />
-          {errors.content && <span className="text-red-500">{errors.content.message}</span>}
-          
-          <p className="text-gray-500 text-sm mt-2">
+        <textarea
+          {...register('content')}
+          className="resize-none focus:outline-none focus:shadow-outline w-full"
+          placeholder="Enter your ideas here..."
+          defaultValue={idea.content}
+          onChange={(e) => {
+            const newContent = e.target.value;
+            setCharCount(newContent.length);
+          }}
+        />
+        {errors.content && <span className="text-red-500">{errors.content.message}</span>}
+
+        <p className="text-gray-500 text-sm mt-2">
           Remaining Characters: {charCount}/150
-          </p>
-        </div>
-        
-        <div className="flex justify-between items-center mt-4">
-          <p className="text-gray-500 text-sm">
-            Created: {idea.createdTime ? new Date(idea.createdTime).toLocaleString() : 'N/A'} | Updated:{' '}
-            {idea.updatedTime ? new Date(idea.createdTime).toLocaleString() : 'N/A'}
-          </p>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 focus:outline-none focus:shadow-outline"
-            onClick={() => handleSubmit(onSubmit)()}
-          >
-            Save
-          </button>
-        </div>
-       </form>
+        </p>
+      </div>
+      <div className="flex justify-between items-center mt-4">
+        <p className="text-gray-500 text-sm">
+          Created: {idea.createdTime ? new Date(idea.createdTime).toLocaleString() : 'N/A'} | Updated:{' '}
+          {idea.updatedTime ? new Date(idea.createdTime).toLocaleString() : 'N/A'}
+        </p>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 focus:outline-none focus:shadow-outline"
+          onClick={() => handleSubmit(onSubmit)()}
+        >
+          Save {isSaved && <div className="absolute bottom-0 left-0 p-2 text-green-500">✓ Saved</div>}
+        </button>
+      </div>
+    </form>
   );
 };
 
