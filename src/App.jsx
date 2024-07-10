51b28f66a7fde0d3744a83aa3665240a998f685b
Malcolm Mutare <135226305+mutaremalcolm@users.noreply.github.com>
2024-07-10 13:56:40 +0200

    Pull Request: Implement IdeaCard Component and Modal with User Input Form (#56)
    * feat: Rename app to Brainwave and enhance UI/UX
    
    - Renamed the application from Thinkpad to Brainwave
    - Implemented a new navigation component with reusable and dynamic elements
    - Refactored the look and feel of the app for a more modern UI
    - Added external links for improved accessibility
    
    * feat: Implement mobile navigation bar with specific mobile view styles
    
    - Added a new navigation bar tailored for mobile devices
    - Applied specific styles to enhance mobile view experience
    
    * feat: Implement IdeaCard component and modal with user input form
    
    - Added IdeaCard component to display ideas with title, description, and timestamps.
    - Implemented modal using shadcn/ui's Dialog component to capture user input.
    - Integrated react-hook-form with zod validation to ensure proper input handling and validation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import ButtonGradient from './assets/svg/ButtonGradient';
 import Header from './components/Header';
+import NewIdeaModal from './components/NewIdeaModal';
+
 
 const App = () => {
   return (
-    <> 
+    <>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header />
       </div>
-      <ButtonGradient/> 
+      <div className='flex flex-col items-center mt-5'>
+        <NewIdeaModal />
+      </div>
+      <ButtonGradient />
     </>
   )
 }
