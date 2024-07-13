1d2597acf0075b1931e8202bf696bf12758c9aef
“malcolmmutare” <mutarem@gmail.com>
2024-07-13 22:08:17 +0200

    feat: rename app to clearscore, refactor navbar, refine newIdeaModal, and add partial localStorage support
    - Renamed the application to 'clearscore' for simplicity and clarity.
    - Refactored the navbar for improved usability and appearance in both large and mobile views.
    - Refined the NewIdeaModal component for better user experience and form handling.
    - Partially implemented the ability to persist ideas by storing them in localStorage, ensuring ideas are not lost on page refresh.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ButtonGradient from './assets/svg/ButtonGradient';
 import Header from './components/Header';
 import NewIdeaModal from './components/NewIdeaModal';
@@ -6,12 +6,22 @@ import IdeaCard from './components/IdeaCard';
 
 const App = () => {
   const [ideas, setIdeas] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false); // State to manage modal visibility
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const addIdea = (newIdea) => {
-    setIdeas([...ideas, newIdea]);
-    setIsModalOpen(false); // Close the modal after adding the idea
-  };
+  // Read from localStorage when the component mounts
+  useEffect(() => {
+    const storedIdeas = localStorage.getItem('ideas');
+    console.log('Retrieved from localStorage:', storedIdeas); // Log to verify
+    if (storedIdeas) {
+      setIdeas(JSON.parse(storedIdeas));
+    }
+  }, []);
+
+  // Save to localStorage whenever the ideas array changes
+  useEffect(() => {
+    console.log('Saving to localStorage:', ideas); // Log to verify
+    localStorage.setItem('ideas', JSON.stringify(ideas));
+  }, [ideas]);
 
   const deleteIdea = (index) => {
     const updatedIdeas = ideas.filter((_, i) => i !== index);
@@ -26,13 +36,18 @@ const App = () => {
     setIsModalOpen(false); // Close the modal
   };
 
+  const addIdea = (newIdea) => {
+    setIdeas((prevIdeas) => [...prevIdeas, newIdea]);
+    setIsModalOpen(false); // Close the modal after adding the idea
+  };
+
   return (
     <>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header openModal={openModal} />
       </div>
       <NewIdeaModal isOpen={isModalOpen} addIdea={addIdea} closeModal={closeModal} />
-      <div className='flex flex-col items-center mt-5'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-5 ml-5 mr-5 px-4'>
         {ideas.map((idea, index) => (
           <IdeaCard key={index} index={index} title={idea.title} description={idea.description} deleteIdea={deleteIdea} />
         ))}
